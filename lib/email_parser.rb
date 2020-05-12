b27fe32248d22009b07304f2c59386fb4546df18
Cailie Skelton <cskelton707@gmail.com>
2020-05-12 16:55:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,15 +1,21 @@
 class EmailParser
 
-attr_accessor :email
-  
-  def  initialize(emails)
-    @email = emails
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
   end
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
   end
 
 end
+
+emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
+parser = EmailParser.new(emails)
+
+puts parser.parse
