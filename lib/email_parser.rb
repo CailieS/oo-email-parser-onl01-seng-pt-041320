6af3a69614ed1d819403f5d0a5e55b93b527b9bf
Cailie Skelton <cskelton707@gmail.com>
2020-05-12 16:55:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,38 +1,15 @@
-class CashRegister
-  attr_accessor :total, :discount, :price, :items
+class EmailParser
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
+attr_accessor :email
+  
+  def  initialize(emails)
+    @email = emails
   end
 
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
-  end
-
-  def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
-      return "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    @total -= @price
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
 
 end
